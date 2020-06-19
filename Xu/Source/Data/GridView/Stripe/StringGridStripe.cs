f604aa521593c85bab7262215617b2d0b846e09e
Xu Li <me@xuli.us>
2020-06-18 19:37:12 -0700

    StringGridStripe

---
+++ Xu/Source/Data/GridView/Stripe/StringGridStripe.cs
@@ -0,0 +1,30 @@
+﻿/// ***************************************************************************
+/// Shared Libraries and Utilities
+/// Copyright 2001-2008, 2014-2020 Xu Li - me@xuli.us
+/// 
+/// ***************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Windows.Media.TextFormatting;
+
+namespace Xu.GridView
+{
+    public class StringGridStripe : GridStripe
+    {
+        public StringColumn Column { get; set; }
+
+        public override void Draw(Graphics g, Rectangle bound, ITable table, int index)
+        {
+            if (table is IStringTable ct)
+            {
+                string value = ct[index, Column]; // It might be some other object
+                g.DrawString(value, Main.Theme.Font, Theme.ForeBrush, bound.Location);
+            }
+        }
+    }
+}

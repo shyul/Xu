62383b17ae60faf95e16bd738b1cce141561c337
Xu Li <me@xuli.us>
2020-07-07 08:14:32 -0700

    Task Tool

---
+++ Xu/Source/Tools/Task.cs
@@ -0,0 +1,38 @@
+﻿/// ***************************************************************************
+/// Shared Libraries and Utilities
+/// Copyright 2001-2008, 2014-2020 Xu Li - me@xuli.us
+/// 
+/// Task Tools
+/// 
+/// ***************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xu
+{
+    public static class TaskTool
+    {
+
+
+        public static bool Continue(this CancellationTokenSource cts)
+        {
+            if (cts is null)
+                return true;
+            else
+                return !cts.IsCancellationRequested;
+        }
+
+        public static bool Cancelled(this CancellationTokenSource cts)
+        {
+            if (cts is null)
+                return false;
+            else
+                return cts.IsCancellationRequested;
+        }
+    }
+}

f2739861a8e8152667fdb695e9b4b95d71cec20f
shyul <me@xuli.us>
2021-03-20 22:08:02 -0400

    IChartAnalysis

---
+++ Xu/Source/Data/Chart/_Types/IChartAnalysis.cs
@@ -0,0 +1,20 @@
+﻿/// ***************************************************************************
+/// Pacmio Research Enivironment
+/// Copyright 2001-2008, 2014-2021 Xu Li - me@xuli.us
+/// 
+/// Technical Analysis Chart UI
+/// 
+/// ***************************************************************************
+
+
+namespace Xu.Chart
+{
+    public interface IChartAnalysis
+    {
+        string AreaName { get; }
+
+        bool ChartEnabled { get; set; }
+
+        int DrawOrder { get; set; }
+    }
+}
